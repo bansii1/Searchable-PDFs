7271dc918b17be34712d2b6a51760c8868086852
bansii1 <bansi.derashri@gmail.com>
2019-04-19 18:06:14 +0530

    files
    base files

---
+++ convertPDFtojpg.py
@@ -0,0 +1,33 @@
+from pdf2image import convert_from_path, convert_from_bytes
+from pdf2image.exceptions import ( PDFInfoNotInstalledError, PDFPageCountError, PDFSyntaxError)
+import glob, os
+from PIL import Image
+import cv2
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+PATH="/home/bansi/Accobots/Using tesseract onl/"
+OPT_PATH="images/"
+	
+def convert_pdf_to_images(filename):
+	
+	basename=filename.split(".")[0]
+	image = convert_from_path(PATH+filename)
+	ct=0
+	for im in image:
+		im.save(OPT_PATH+basename+str(ct)+".jpg", quality=100)
+		ct=ct+1
+		#image[0].save(str(i)+".jpg")
+	print("done converting pdf to images")
+
+def merge_pdfs(OPT_PATH):
+
+	filelist= [file for file in os.listdir(OPT_PATH) if file.endswith('.pdf')]
+	output = PdfFileWriter()
+	#print(filelist)
+	for file in filelist:
+		pdfOne = PdfFileReader(open(OPT_PATH+file, "rb"))
+		output.addPage(pdfOne.getPage(0))
+		
+	outputStream = open(r"output.pdf", "wb")
+	output.write(outputStream)
+	outputStream.close()
---
+++ extractData.py
@@ -0,0 +1,73 @@
+from PIL import Image
+import argparse
+import cv2
+import os
+import pytesseract
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import ImageEnhance, ImageFilter
+from convertPDFtojpg import convert_pdf_to_images, merge_pdfs
+import sys
+from wand.image import Image as WImage
+
+target_width=540
+target_height=960
+kernel=np.ones((3,3),np.uint8)
+PATH="/home/bansi/Accobots/Using tesseract onl/"
+OPT_PATH="images/"
+
+
+
+def get_from_pdf(pdf_name):
+	
+	convert_pdf_to_images(pdf_name)
+
+	filelist= [file for file in os.listdir(OPT_PATH) if file.endswith('.jpg')]
+	print(filelist)
+	
+	ct = 0
+	for file in filelist:
+
+		pdfopt = pytesseract.image_to_pdf_or_hocr(OPT_PATH+file, extension='pdf')
+		file1 = open('temp.txt', 'wb')
+		file1.write(pdfopt)
+		file1.close()
+
+		file2 = open(OPT_PATH+'optfile'+pdf_name.split(".")[0]+".pdf", 'a+b')
+		for line in open('temp.txt', 'rb').readlines():
+			file2.write(line)
+		print("writing to "+OPT_PATH+'optfile'+pdf_name.split(".")[0]+str(ct)+".pdf")
+		ct =ct+1
+		
+	#merge pdf has errors in it... NEEDS MODIFICATION
+	#merge_pdfs(OPT_PATH)
+
+def get_from_image(image_name):
+	
+	pdfopt = pytesseract.image_to_pdf_or_hocr(OPT_PATH+image_name, extension='pdf')
+	file1 = open('temp.txt', 'wb')
+	file1.write(pdfopt)
+	file1.close()
+
+	file2 = open(OPT_PATH+'optfile'+image_name.split(".")[0]+".pdf", 'a+b')
+	for line in open('temp.txt', 'rb').readlines():
+		file2.write(line)
+	
+	print("writing to "+OPT_PATH+'optfile'+image_name.split(".")[0]+".pdf")
+
+	
+
+	
+ap=argparse.ArgumentParser()
+ap.add_argument("-f","--filename", help="Path to input file-pdf/image ")
+args=vars(ap.parse_args())
+filename=args["filename"]
+
+if filename.split(".")[-1]=="pdf":
+	get_from_pdf(filename)
+elif filename.split(".")[-1]=="jpg" or filename.split(".")[-1]=="jpeg" or filename.split(".")[-1]=="png":
+	get_from_image(filename)
+else:
+	print()
+print("Done")
+																																																																																																																																																												
